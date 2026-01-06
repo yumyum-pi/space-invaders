85af5de744ee12e8a4bd73aeaa2e3d5374ac88b4
Vivek Rawat <vivek.rawat888@gmail.com>
2026-01-06 13:15:47 +0530

    added todo and enemey

--- src/main.c
+++ src/main.c
@@ -2,14 +2,20 @@
 #include "./renderer.h"
 #include "game_update.h"
 #include "input.h"
+#include "utils/math.h"
 #include <unistd.h>
 
 int main() {
   renderer r = renderer_init();
   GameState gs = game_state_init(r.terminal_size, "This is title");
 
+  // TODO: create enemies
+  // - Add them in the world
+  // - make them move over time
   // TODO: need to properly handle non blocking inputs
   input_init();
+  gs.enemy.position = (Vec2i){r.terminal_size.x / 2, 4};
+
   while (gs.is_running) {
     frame_begin(&gs);
     // get the inputs
