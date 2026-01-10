81cebb292d452d838cd0f6fd6667fd11fdd9b6fa
Vivek Rawat <vivek.rawat888@gmail.com>
2026-01-10 09:18:01 +0530

    added gun type

--- src/player.c
+++ src/player.c
@@ -5,5 +5,17 @@ Player new_player() {
   return (Player){
       .position = zero_vec(),
       .velocity = zero_vec(),
+      .gun = new_gun_default(),
   };
 }
+
+Gun new_gun(int fire_rate, int reload_rate) {
+  Gun g = {
+      .fire_rate = fire_rate,
+      .relaod_rate = reload_rate,
+      .last_fired_frame = 0,
+  };
+  return g;
+};
+
+Gun new_gun_default(void) { return new_gun(1, 5); }
--- src/player.h
+++ src/player.h
@@ -4,9 +4,20 @@
 #include "./utils/math.h"
 
 typedef struct {
+  int fire_rate;
+  int relaod_rate;
+  int last_fired_frame;
+} Gun;
+
+typedef struct {
   Vec2i position;
   Vec2i velocity;
+  Gun gun;
 } Player;
 
 Player new_player(void);
+
+Gun new_gun(int fire_rate, int reload_rate);
+Gun new_gun_default(void);
+
 #endif // !PLAYER_H
