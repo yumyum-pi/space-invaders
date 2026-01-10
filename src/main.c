41a9ac0e686b095dc82960583804b9d79823bcaf
Vivek Rawat <vivek.rawat888@gmail.com>
2026-01-10 09:11:10 +0530

    added to do this

--- src/main.c
+++ src/main.c
@@ -11,6 +11,11 @@ int main() {
   // TODO: need to properly handle non blocking inputs
   input_init();
 
+  // TODO: make mulitple bullets
+  // - player ship wil have a fire rate: 24 frames per seconds - 1 bullet per
+  // second
+  // - player should have 10 rounds of bullets
+  // - after 10 rounds the player will relaod which will take 4 seconds
   while (gs.is_running) {
     frame_begin(&gs);
     // get the inputs
